a95b814780810c77752df0756ccb37ef67d38596
LSAfrika <lstudiosafrika@gmail.com>
2023-09-18 14:47:13 +0300

    added route id to product route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }, 
-  { path: 'product', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule) }, 
+  { path: 'product/:productid', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule) }, 
   { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }, 
   { path: 'cart', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartModule) },
   { path: 'brands', loadChildren: () => import('./pages/brands/brands.module').then(m => m.BrandsModule) },
