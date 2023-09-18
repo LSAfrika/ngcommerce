fc3ca421d6f9951436845df87e63df6b93c895da
LSAfrika <lstudiosafrika@gmail.com>
2023-09-18 14:46:52 +0300

    moved categories and brands moc data from ui to catbrand service

--- src/app/services/brandsandcategories.service.ts
+++ src/app/services/brandsandcategories.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Brands } from '../interfaces/brands';
+import { Categories } from '../interfaces/categories';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandsandcategoriesService {
 
+
+  brand='brand'
+  brands:Brands[]=[
+    {brand:'Sony',icon:'../../../assets/brands/sony.png'},
+    {brand:'samsung',icon:'../../../assets/brands/samsung.png'},
+    {brand:'apple',icon:'../../../assets/brands/apple.webp'},
+    {brand:'dell',icon:'../../../assets/brands/dell.webp'},
+    {brand:'Lg',icon:'../../../assets/brands/lg.png'},
+    {brand:'lenovo',icon:'../../../assets/brands/lenovo.png'},
+    {brand:'hisense',icon:'../../../assets/brands/hisense.png'},
+    {brand:'Hp',icon:'../../../assets/brands/hp.png'},
+    {brand:'Others',icon:'../../../assets/brands/others.svg'},
+    // {brand:'phillips',icon:'../../../assets/brands/'},
+  ]
+category='categories'
+
+  categories:Categories[]=[
+    {category:' All',icon:'../../../assets/categories/allcat.svg'},
+    {category:'Phones',icon:'../../../assets/categories/mobile.svg'},
+    {category:'Laptops',icon:'../../../assets/categories/laptop.svg'},
+    {category:'Desktops',icon:'../../../assets/categories/computer.svg'},
+    {category:'Tvs',icon:'../../../assets/categories/tv.svg'},
+    {category:'HomeTheaters',icon:'../../../assets/categories/hometheater.svg'},
+    {category:'Tablets',icon:'../../../assets/categories/tablet.svg'},
+
+
+  ]
   constructor() { }
 }
--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -7,30 +7,6 @@ import { Categories } from '../interfaces/categories';
 })
 export class UiService {
 
-brand='brand'
-  brands:Brands[]=[
-    {brand:'Sony'},
-    {brand:'samsung'},
-    {brand:'apple'},
-    {brand:'dell'},
-    {brand:'Lg'},
-    {brand:'lenovo'},
-    {brand:'acer'},
-    {brand:'phillips'},
-    {brand:'hisense'},
-    {brand:'Hp'},
-  ]
-category='categories'
 
-  categories:Categories[]=[
-    {category:'Phones'},
-    {category:'Laptops'},
-    {category:'Desktops'},
-    {category:'Tvs'},
-    {category:'HomeTheaters'},
-    {category:'Tablests'},
-
-
-  ]
   constructor() { }
 }
