0c9434709d505eba3602cbe79089f8b9b4ccb999
LSAfrika <lstudiosafrika@gmail.com>
2023-09-29 15:51:02 +0300

    retrieved store id from query params

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-store',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class StoreComponent {
 
+  private activeroute=inject(ActivatedRoute)
+  _storeid=''
+constructor(){
+this.activeroute.queryParams.pipe(filter(params=>params['storeid'])).subscribe(res=>{this._storeid=res['storeid'];console.log(this._storeid);}
+)
+
+}
 }
