1ca460de8d124756305c8e9c102641b2f7ea15cf
LSAfrika <lstudiosafrika@gmail.com>
2023-09-30 12:26:10 +0300

    created admin page for store owners

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'cart', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartModule) },
   { path: 'brands', loadChildren: () => import('./pages/brands/brands.module').then(m => m.BrandsModule) },
   { path: 'store', loadChildren: () => import('./pages/store/store.module').then(m => m.StoreModule) },
+  { path: 'adminpanel', loadChildren: () => import('./pages/adminpanel/adminpanel.module').then(m => m.AdminpanelModule) },
   { path: '**', loadChildren: () => import('./pages/nf404/nf404.module').then(m => m.Nf404Module) }];
 
 @NgModule({
