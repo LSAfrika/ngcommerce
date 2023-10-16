99d194f2434d603f8ea585589c260403e0233676
LSAfrika <lstudiosafrika@gmail.com>
2023-10-16 15:57:35 +0300

    working on profile ui page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UiService } from 'src/app/services/frontendservices/ui.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  public uiservice=inject(UiService)
 }
