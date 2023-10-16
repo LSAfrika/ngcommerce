a5866c9615712ca270b849cf566bb01fae2cd1fb
LSAfrika <lstudiosafrika@gmail.com>
2023-10-16 14:47:13 +0300

    added auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShareduiModule } from './modules/sharedui/sharedui.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+// import { AuthInterceptor } from './guards/auth.interceptor';
 
 
 @NgModule({
@@ -15,7 +16,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,ShareduiModule,HttpClientModule
   ],
-  providers: [],
+  providers: [
+    // {provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor,multi:true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
