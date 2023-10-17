98e0961165e5c8a3a88343800ebabd065241dd2a
LSAfrika <lstudiosafrika@gmail.com>
2023-10-17 13:04:31 +0300

    finished router guard logic

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,51 +1,53 @@
 
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree,Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, CanDeactivate, RouterStateSnapshot, UrlTree,Router, Route } from '@angular/router';
+import { map, Observable, of, Subject, tap } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+import { User } from '../interfaces/user.interface';
+import { UserService } from '../services/endpoints/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 // , CanDeactivate<unknown>
 export class LoginGuard implements CanActivate {
-constructor(private router:Router){}
+constructor(private router:Router,private usersvc:UserService){}
 
-  canActivate() {
+ userstate:Observable<boolean>=of(!!localStorage.getItem('ecomtoken'))
+ endcheck$=new Subject()
+  canActivate():Observable<boolean>|boolean {
    // this.router.navigateByUrl('/')
 
-   if(!!localStorage.getItem('token') ) {
 
-    const tokenavailable=localStorage.getItem('token')
-    if(tokenavailable!=undefined) {
+       return this.userstate.pipe(map((res)=>{
+        if(res==true) {
+
+    const tokenavailable=localStorage.getItem('ecomtoken')
+
       let token:string
-    const storetoken=localStorage.getItem('token')
-      if(storetoken){
-
-        token=storetoken.split('.')[1]
-        const userbio= atob(token)
-      } 
-
-  
 
 
+        token=tokenavailable?.split('.')[1]||''
+        const userbio= JSON.parse(atob(token))
 
-     
+        delete  userbio.exp
+        delete  userbio.iat
 
-    return true;}
-    else{
+
+        this.usersvc.user=userbio as User
 
 
 
-// this.io.disconnectinstance()
-this.router.navigateByUrl('/login')
-return false
+          return true}
+        else{
+// window.location.assign('/login')
+          this.router.navigate(['/login'])
+          console.log('failed to resolve');
 
-    }
-  }else{
+          return false
+        }
+      }))
 
-    this.router.navigateByUrl('/login')
-    return false
-  }
 
 
   }
