c54c163382dd897d63b2c0267bf1dac066126c67
LSAfrika <lstudiosafrika@gmail.com>
2023-10-17 13:13:21 +0300

    added router guard to auth routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }, 
-  { path: 'product/:productid', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule) }, 
-  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }, 
-  { path: 'cart', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartModule) },
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
   { path: 'brands', loadChildren: () => import('./pages/brands/brands.module').then(m => m.BrandsModule) },
   { path: 'store', loadChildren: () => import('./pages/store/store.module').then(m => m.StoreModule) },
   { path: 'admin', loadChildren: () => import('./pages/adminpanel/adminpanel.module').then(m => m.AdminpanelModule) },
+  { path: 'product/:productid', loadChildren: () => import('./pages/product/product.module').then(m => m.ProductModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule),canActivate:[LoginGuard] },
+  { path: 'cart', loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartModule),canActivate:[LoginGuard] },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: '**', loadChildren: () => import('./pages/nf404/nf404.module').then(m => m.Nf404Module) }];
 
